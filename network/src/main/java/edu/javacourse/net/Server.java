b6c4371f5ccb06f1a59e5a2e0c1e62c43ea8b6bd
hgxey6 <hgxey6@gmail.com>
2021-01-18 20:28:40 +0500

    update server simple

--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -1,40 +1,102 @@
 package edu.javacourse.net;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
-public class Server {
+public class Server
+{
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ServerSocket socket = new ServerSocket(25225, 200);
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(25225);
+        Map<String, Greetable> handlers = loadHandlers();
 
-        System.out.println("server is started");
-
-        while (true) {
+        System.out.println("Server is started");
+        while(true) {
             Socket client = socket.accept();
-            handleRequest(client);
+            new SimpleServer(client, handlers).start();
         }
     }
 
-    private static void handleRequest(Socket client) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+    private static Map<String, Greetable> loadHandlers() {
+        Map<String, Greetable> result = new HashMap<>();
 
-        StringBuilder sb = new StringBuilder("Hello, ");
-        String username = br.readLine();
+        try (InputStream is = Server.class.getClassLoader().getResourceAsStream("server.properties")) {
 
-        System.out.println("server got string: " + username);
+            Properties properties = new Properties();
+            properties.load(is);
 
-        sb.append(username);
+            for(Object command : properties.keySet()) {
+                String className = properties.getProperty(command.toString());
+                Class<Greetable> cl = (Class<Greetable>) Class.forName(className);
+                Greetable handler = cl.getConstructor().newInstance();
+                result.put(command.toString(), handler);
+            }
 
-        bw.write(sb.toString());
-        bw.newLine();
-        bw.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
 
-        br.close();
-        bw.close();
 
-        client.close();
+        return result;
+    }
+}
+
+class SimpleServer extends Thread {
+
+    private Socket client;
+    private Map<String, Greetable> handlers;
+
+    public SimpleServer(Socket client, Map<String, Greetable> handlers) {
+        this.client = client;
+        this.handlers = handlers;
+    }
+
+    public void run() {
+        handleRequest();
+    }
+
+    private void handleRequest() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            String request = br.readLine();
+            String[] lines = request.split("\\s+");
+            String command = lines[0];
+            String userName = lines[1];
+            System.out.println("Server got string 1:" + command);
+            System.out.println("Server got string 2:" + userName);
+//            Thread.sleep(2000);
+
+            String response = buildResponse(command, userName);
+            bw.write(response);
+            bw.newLine();
+            bw.flush();
+
+            br.close();
+            bw.close();
+
+            client.close();
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private String buildResponse(String command, String userName) {
+        Greetable handler = handlers.get(command);
+        if (handler != null) {
+            return handler.buildResponse(userName);
+        }
+        return "Hello, " + userName;
     }
 }
